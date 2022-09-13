d6798162509873698abc80a0c8a6877ce63116dc
Fahad <76650975+mediocre9@users.noreply.github.com>
2022-09-13 09:23:24 +0500

    renaming file structures

--- lib/screens/user_screen/cubit/user_cubit_exports.dart
+++
@@ -1,7 +0,0 @@
-// dependency exports....
-export 'package:bloc/bloc.dart';
-export 'package:equatable/equatable.dart';
-
-// app exports...
-export 'package:khata/constants.dart';
-export 'package:khata/models/model/user_model.dart';
