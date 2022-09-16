a1b91b2bcc346d81850dcf11c9abdec6aa32b99a
Fahad <76650975+mediocre9@users.noreply.github.com>
2022-09-16 07:57:55 +0500

    'same placed orders couldn't be completed'

---
+++ order_screen/presentation/order_screen.dart
@@ -0,0 +1,381 @@
+import 'package:flutter/cupertino.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:khata/constants.dart';
+import 'package:khata/models/model/order.dart';
+import 'package:khata/screens/order_screen/cubit/order_cubit.dart';
+import 'package:khata/screens/order_screen/sub_screens/order_detail_screen/cubit/order_detail_cubit.dart';
+import 'package:khata/screens/order_screen/sub_screens/order_detail_screen/presentation/order_detail_screen.dart';
+import 'package:khata/themes/decorations.dart';
+import 'package:khata/widgets/custom_app_bar.dart';
+import 'package:khata/widgets/custom_card.dart';
+import 'package:khata/widgets/custom_drawer.dart';
+import 'package:khata/widgets/empty_record.dart';
+import 'package:khata/widgets/tray_container.dart';
+import 'package:khata/widgets/record_not_found.dart';
+import 'package:khata/widgets/sub_screen_navigator_widget.dart';
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: const CustomAppBar(
+          title: Text("MANAGE"),
+          subtitle: Text("ORDERS"),
+        ),
+        endDrawer: const CustomDrawer(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10,
+            vertical: 8,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SubScreenNavigatorWidget(
+                label: "ADD ORDER",
+                route: '/AddOrderScreen',
+              ),
+              const SearchFieldAreaWidget(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  TotalOrders(),
+                  PendingOrders(),
+                ],
+              ),
+              const TotalGain(),
+              const SizedBox(height: 10),
+              const Text(
+                "RECENT ORDERS",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const TrayContainer(
+                stateManager: OrderInterfaceStateManager(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchFieldAreaWidget extends StatelessWidget {
+  const SearchFieldAreaWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Text(
+              "SEARCH ORDER",
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+            const Icon(
+              Icons.search_rounded,
+              size: 14,
+            ),
+          ],
+        ),
+        const SizedBox(height: 4),
+        TextField(
+          style: const TextStyle(color: kTextColor),
+          decoration: const InputDecoration(hintText: "Search"),
+          onChanged: (search) {
+            BlocProvider.of<OrderCubit>(context).searchOrder(search);
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class OrderInterfaceStateManager extends StatelessWidget
+    implements InterfaceStateManager {
+  const OrderInterfaceStateManager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<OrderCubit, OrderState>(
+      builder: (context, state) {
+        if (state is OrderInitialState) {
+          return EmptyRecordsWidget(
+            message: state.message,
+            icon: state.icon,
+          );
+        } else if (state is OrderLoadState) {
+          return ScrollConfiguration(
+            behavior: ScrollConfiguration.of(context).copyWith(
+              dragDevices: {
+                PointerDeviceKind.touch,
+                PointerDeviceKind.mouse,
+              },
+            ),
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: state.listOfOrders.length,
+              physics: const AlwaysScrollableScrollPhysics(
+                parent: BouncingScrollPhysics(),
+              ),
+              itemBuilder: (context, index) {
+                return OrderCard(
+                  order: state.listOfOrders[index],
+                  index: index,
+                );
+              },
+            ),
+          );
+        } else if (state is OrderSearchState) {
+          return ListView.builder(
+            itemCount: state.listOfOrders.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return OrderCard(
+                order: state.listOfOrders[index],
+                index: index,
+              );
+            },
+          );
+        } else if (state is OrderFoundState) {
+          return RecordNotFoundWidget(
+            message: state.message,
+          );
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+}
+
+class TotalOrders extends StatelessWidget {
+  const TotalOrders({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: CustomCard(
+        shadow: false,
+        borderRadius: 0,
+        height: 110,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "TOTAL",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Text(
+              BlocProvider.of<OrderCubit>(context).totalOrders.toString(),
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Text(
+              "ORDERS",
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PendingOrders extends StatelessWidget {
+  const PendingOrders({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(
+        color: Colors.white,
+        child: Container(
+          height: 90,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 3,
+              color: const Color.fromRGBO(58, 52, 98, 1),
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "PENDING",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: const Color.fromRGBO(58, 52, 98, 1)),
+              ),
+              Text(
+                BlocProvider.of<OrderCubit>(context).totalPendingOrders.toString(),
+                style: Theme.of(context)
+                    .textTheme
+                    .headline5!
+                    .copyWith(color: const Color.fromRGBO(58, 52, 98, 1)),
+              ),
+              Text(
+                "ORDERS",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleSmall!
+                    .copyWith(color: const Color.fromRGBO(58, 52, 98, 1)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TotalGain extends StatelessWidget {
+  const TotalGain({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 10,
+            ),
+            color: const Color.fromRGBO(22, 60, 98, 1),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "TOTAL GAIN",
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelLarge!
+                      .copyWith(color: const Color.fromRGBO(215, 215, 255, 1)),
+                ),
+                Text(
+                  "${BlocProvider.of<OrderCubit>(context).totalGainedMoney.toString()} PKR",
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class OrderCard extends StatelessWidget with GradientDecoration {
+  final int index;
+  final Order order;
+  const OrderCard({
+    Key? key,
+    required this.index,
+    required this.order,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        decoration: gradientDecoration(),
+        child: ListTile(
+          title: Text(
+            order.productName!.toUpperCase(),
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "RS. ${order.cost}",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: const Color.fromARGB(255, 218, 224, 236)),
+              ),
+              if (order.pendingStatus!) ...[
+                OrderStatusIcon(
+                  data: order.customerName!,
+                  icon: CupertinoIcons.exclamationmark_circle,
+                  color: Colors.yellow,
+                ),
+              ] else ...[
+                OrderStatusIcon(
+                  data: order.customerName!,
+                  icon: Icons.check_circle_rounded,
+                  color: Colors.lightGreenAccent,
+                ),
+              ],
+            ],
+          ),
+          enabled: true,
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => BlocProvider(
+                  create: (context) => OrderDetailCubit(),
+                  child: OrderDetailScreen(
+                    order: Order(
+                      order.customerName,
+                      order.productName,
+                      order.cost,
+                      order.createdDate,
+                      order.completedDate,
+                      order.pendingStatus,
+                    ),
+                    index: index,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class OrderStatusIcon extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String data;
+
+  const OrderStatusIcon({
+    Key? key,
+    required this.color,
+    required this.icon,
+    required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          data.trim().toUpperCase(),
+          style: Theme.of(context)
+              .textTheme
+              .titleSmall!
+              .copyWith(color: const Color.fromARGB(255, 218, 224, 236)),
+        ),
+        Icon(icon, color: color),
+      ],
+    );
+  }
+}
---
+++ order_screen/sub_screens/order_detail_screen/presentation/order_detail_screen.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:khata/constants.dart';
+import 'package:khata/models/model/order.dart';
+import 'package:khata/screens/order_screen/sub_screens/order_detail_screen/cubit/order_detail_cubit.dart';
+import 'package:khata/widgets/custom_app_bar.dart';
+import 'package:khata/widgets/custom_card.dart';
+import 'package:khata/widgets/custom_drawer.dart';
+import 'package:khata/widgets/custom_outlined_button.dart';
+
+class OrderDetailScreen extends StatelessWidget {
+  final Order order;
+  final int index;
+  const OrderDetailScreen({
+    Key? key,
+    required this.order,
+    required this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    BlocProvider.of<OrderDetailCubit>(context).init(order, index);
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      endDrawerEnableOpenDragGesture: true,
+      appBar: const CustomAppBar(
+        title: Text("Order"),
+        subtitle: Text("Book"),
+      ),
+
+      // Drawer
+      endDrawer: const CustomDrawer(),
+      body: Center(
+        child: CustomCard(
+          innerMainAlignment: MainAxisAlignment.start,
+          innerCrossAlignment: CrossAxisAlignment.start,
+          width: double.maxFinite,
+          shadow: true,
+          height: 350,
+          horizontalMargin: 30,
+          elevationLevel: 5,
+          borderRadius: 5,
+          child: Container(
+            padding: const EdgeInsets.only(left: 30, right: 8, top: 2),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      color: Colors.white60,
+                      icon: const Icon(Icons.close_outlined),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.only(bottom: 30, left: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        order.productName!.toUpperCase(),
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 218, 224, 236),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const SizedBox(height: 3),
+                      Text(
+                        "RS. ${order.cost!}",
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 3),
+                      Text(
+                        order.customerName!.toUpperCase(),
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 218, 224, 236),
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                OrderStatusWidget(status: order.pendingStatus!),
+                const SizedBox(height: 5),
+                Text(
+                  "CREATED DATE : ${DateUtils.dateOnly(order.createdDate!).toString().replaceRange(10, null, "")}",
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 218, 224, 236),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  "COMPLETED DATE : ${DateUtils.dateOnly(order.completedDate!).toString().replaceRange(10, null, "")}",
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 218, 224, 236),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      BlocBuilder<OrderDetailCubit, OrderDetailState>(
+                        builder: (context, state) {
+                          if (state is UnMarkedOrderState) {
+                            return OutlinedButton(
+                              child: const Text("MARK COMPLETED"),
+                              onPressed: () {
+                                BlocProvider.of<OrderDetailCubit>(context)
+                                    .completeOrder();
+
+                                Navigator.pushReplacementNamed(
+                                    context, '/OrderScreen');
+                              },
+                            );
+                          } else {
+                            return Container(height: 35);
+                          }
+                        },
+                      ),
+                      const SizedBox(height: 7),
+                      CustomOutlinedButton(
+                        text: "DELETE",
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text(
+                                  "Warning!",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleLarge!
+                                      .copyWith(color: Colors.black),
+                                ),
+                                content: Text(
+                                  "Do you really want to delete this record?",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleMedium!
+                                      .copyWith(color: Colors.black),
+                                ),
+                                actions: [
+                                  TextButton(
+                                    child: const Text("Yes"),
+                                    onPressed: () {
+                                      orderBox!.deleteAt(index);
+                                      Navigator.pushReplacementNamed(
+                                          context, '/OrderScreen');
+                                      ScaffoldMessenger.of(context).showSnackBar(
+                                        SnackBar(
+                                          key: UniqueKey(),
+                                          content: const Text(
+                                            "Item deleted!",
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
+                                          backgroundColor: kSnackBarErrorColor,
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text("Cancel"),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class OrderStatusWidget extends StatelessWidget {
+  final bool status;
+  const OrderStatusWidget({
+    Key? key,
+    required this.status,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String message = status ? "PENDING" : "COMPLETED";
+
+    return Text(
+      "STATUS : $message",
+      style: const TextStyle(
+        color: Color.fromARGB(255, 218, 224, 236),
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+}
