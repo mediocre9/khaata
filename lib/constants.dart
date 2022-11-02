0afa0bb20287a73e817e383a1bdb6ea365a47b73
Fahad Zia <76650975+mediocre9@users.noreply.github.com>
2022-11-02 12:55:42 +0500

    refactor: remove dead code

--- lib/constants.dart
+++ lib/constants.dart
@@ -13,17 +13,14 @@ const Color kTealGradient = Color.fromARGB(255, 53, 100, 88);
 const Color kCardTextColor = Color.fromRGBO(255, 255, 255, 1.0);
 const Color kDrawerItemColor = Color.fromRGBO(255, 255, 255, 1.0);
 const Color kDrawerSplashColor = Color.fromARGB(61, 215, 216, 255);
-const double kDrawerItemFontSize = 16.5;
-const double kDrawerSubItemFontSize = 12.0;
 const Color kSnackBarSuccessColor = Color.fromARGB(255, 69, 189, 85);
 const Color kSnackBarErrorColor = Color.fromARGB(255, 223, 71, 71);
+const double kDrawerItemFontSize = 16.5;
+const double kDrawerSubItemFontSize = 12.0;
 
 Box<Customer>? customerBox;
 Box<Product>? productBox;
 Box<Order>? orderBox;
 
-List<Product?> productList = [];
-List<Order?> orderList = [];
-
-const String kAppName = "Khaata";
-const String kCompany = "Smart Alpha Software House";
+const String kAppName = "KHAATA";
+const String kCompany = "SMART ALPHA SOFTWARE";
